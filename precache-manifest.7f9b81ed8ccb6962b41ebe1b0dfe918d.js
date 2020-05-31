a632f024ad8a350740fecadc54898b4da50095ad
v.demidov@muz-lab.ru <16011202>
2020-05-31 17:28:17 +0300

    Updates

---
+++ precache-manifest.7f9b81ed8ccb6962b41ebe1b0dfe918d.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a397e6c659a961c712b216c39360e1ee",
+    "url": "/pizza-test/index.html"
+  },
+  {
+    "revision": "14bf0ed5a3fab053fa35",
+    "url": "/pizza-test/static/js/2.1a429060.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizza-test/static/js/2.1a429060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f495ec3a9b0780719c9",
+    "url": "/pizza-test/static/js/main.c2898a10.chunk.js"
+  },
+  {
+    "revision": "fbc8bd4c9757d9012c32",
+    "url": "/pizza-test/static/js/runtime-main.fcf3dad0.js"
+  },
+  {
+    "revision": "6d1796a9f798ced8961baf3c79f894b6",
+    "url": "/pizza-test/static/media/Montserrat-Black.6d1796a9.ttf"
+  },
+  {
+    "revision": "b5331c5f5aae974d18747a94659ed002",
+    "url": "/pizza-test/static/media/Montserrat-BlackItalic.b5331c5f.ttf"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/pizza-test/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "781190aecb862fffe858d42b124658cc",
+    "url": "/pizza-test/static/media/Montserrat-BoldItalic.781190ae.ttf"
+  },
+  {
+    "revision": "9bc77c3bca968c7490de95d1532d0e87",
+    "url": "/pizza-test/static/media/Montserrat-ExtraBold.9bc77c3b.ttf"
+  },
+  {
+    "revision": "09a2d2564ea85d25a3b3a7903159927b",
+    "url": "/pizza-test/static/media/Montserrat-ExtraBoldItalic.09a2d256.ttf"
+  },
+  {
+    "revision": "38bc5e073a0692a4eddd8e61c821d57a",
+    "url": "/pizza-test/static/media/Montserrat-ExtraLight.38bc5e07.ttf"
+  },
+  {
+    "revision": "6885cd4955ecc64975a122c3718976c1",
+    "url": "/pizza-test/static/media/Montserrat-ExtraLightItalic.6885cd49.ttf"
+  },
+  {
+    "revision": "6786546363c0261228fd66d68bbf27e9",
+    "url": "/pizza-test/static/media/Montserrat-Italic.67865463.ttf"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/pizza-test/static/media/Montserrat-Light.100b38fa.ttf"
+  },
+  {
+    "revision": "428b2306e9c7444556058c70822d7d7c",
+    "url": "/pizza-test/static/media/Montserrat-LightItalic.428b2306.ttf"
+  },
+  {
+    "revision": "a98626e1aef6ceba5dfc1ee7112e235a",
+    "url": "/pizza-test/static/media/Montserrat-Medium.a98626e1.ttf"
+  },
+  {
+    "revision": "287208c81e03eaf08da630e1b04d80e8",
+    "url": "/pizza-test/static/media/Montserrat-MediumItalic.287208c8.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/pizza-test/static/media/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/pizza-test/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "2d3cef91fbb6377e40398891b90d29bf",
+    "url": "/pizza-test/static/media/Montserrat-SemiBoldItalic.2d3cef91.ttf"
+  },
+  {
+    "revision": "0052573bbf05658a18ba557303123533",
+    "url": "/pizza-test/static/media/Montserrat-Thin.0052573b.ttf"
+  },
+  {
+    "revision": "3cb621135b5f6fe15d7c2eba68f0ee37",
+    "url": "/pizza-test/static/media/Montserrat-ThinItalic.3cb62113.ttf"
+  },
+  {
+    "revision": "39709da0a7a65cf8560acad4c8b88ea2",
+    "url": "/pizza-test/static/media/PT_Sans-Web-Bold.39709da0.ttf"
+  },
+  {
+    "revision": "f7840901a0215fbcfe907ab81e5b22dc",
+    "url": "/pizza-test/static/media/PT_Sans-Web-BoldItalic.f7840901.ttf"
+  },
+  {
+    "revision": "ba735c5fcb4a64ff2357066ee51c99d6",
+    "url": "/pizza-test/static/media/PT_Sans-Web-Italic.ba735c5f.ttf"
+  },
+  {
+    "revision": "70fda92429e8a5f2fbaa5f10da118c74",
+    "url": "/pizza-test/static/media/PT_Sans-Web-Regular.70fda924.ttf"
+  },
+  {
+    "revision": "4e4cb6a802affe0fe4b42899fd82f3d8",
+    "url": "/pizza-test/static/media/cart.4e4cb6a8.svg"
+  }
+]);
